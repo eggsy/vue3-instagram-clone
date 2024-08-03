84236f398de3f708c4bc1d7e6360f8205d3a6b51
Abdulbaki Dursun <eggsydev@gmail.com>
2024-08-04 01:41:09 +0300

    fix: camera page

--- src/views/camera.vue
+++ src/views/camera.vue
@@ -9,50 +9,52 @@ import CameraButton from "../components/Camera/Button.vue";
 import CameraCenterMenu from "../components/Camera/CenterMenu.vue";
 
 const { videoInputs: cameras } = useDevicesList({
-  requestPermissions: true,
-  constraints: {
-    audio: false,
-  },
+    requestPermissions: true,
+    constraints: {
+        audio: false,
+    },
 });
 
 const currentCamera = computed(
-  () =>
-    cameras.value.filter((item) => !/(iPhone|OBS)/g.test(item.label))?.[0]
-      ?.deviceId
+    () =>
+        cameras.value.filter((item) => !/(iPhone|OBS)/g.test(item.label))?.[0]
+            ?.deviceId,
 );
 
 const { stream, start, stop } = useUserMedia({
-  audioDeviceId: false,
-  videoDeviceId: currentCamera,
+    constraints: {
+        audio: false,
+        video: { deviceId: currentCamera.value },
+    },
 });
 
 onMounted(() => {
-  start();
+    start();
 });
 
 onBeforeMount(() => {
-  stop();
+    stop();
 });
 </script>
 
 <template>
-  <div
-    class="bg-black relative flex flex-col px-4 py-6 text-white min-h-screen"
-  >
-    <CameraHeader class="z-10" />
+    <div
+        class="bg-black relative flex flex-col px-4 py-6 text-white min-h-screen"
+    >
+        <CameraHeader class="z-10" />
 
-    <div class="flex-grow">
-      <video
-        v-if="stream"
-        :srcObject="stream"
-        autoplay
-        playsinline
-        class="absolute inset-0 h-full object-cover"
-      />
+        <div class="flex-grow">
+            <video
+                v-if="stream"
+                :srcObject="stream"
+                autoplay
+                playsinline
+                class="absolute inset-0 h-full object-cover"
+            />
+        </div>
+
+        <CameraCenterMenu class="z-10" />
+        <CameraButton class="z-10" />
+        <CameraBottomMenu class="z-10" />
     </div>
-
-    <CameraCenterMenu class="z-10" />
-    <CameraButton class="z-10" />
-    <CameraBottomMenu class="z-10" />
-  </div>
 </template>
